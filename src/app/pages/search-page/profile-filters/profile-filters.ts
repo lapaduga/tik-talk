67c4adba9776039277c1f3930a70ad114b6833b6
Denis Kotov <deniskotov29042015@gmail.com>
2025-06-20 16:34:15 +0500

    fix

--- src/app/pages/search-page/profile-filters/profile-filters.ts
+++ src/app/pages/search-page/profile-filters/profile-filters.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnDestroy } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ProfileService } from '../../../data/services/profile.service';
 import { debounceTime, startWith, Subscription, switchMap } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-profile-filters',
