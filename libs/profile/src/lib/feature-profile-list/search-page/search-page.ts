00eacf070e7c2d3fd191a5312880fda169bdddd1
Denis Kotov <deniskotov29042015@gmail.com>
2025-08-02 13:38:45 +0500

    all types of scroll

--- libs/profile/src/lib/feature-profile-list/search-page/search-page.ts
+++ libs/profile/src/lib/feature-profile-list/search-page/search-page.ts
@@ -1,27 +1,65 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { ProfileCard } from '../../ui/index';
 import { ProfileFilters } from '../index';
 import { Store } from '@ngrx/store';
 import { profileActions, selectFilteredProfiles, selectProfileFilters } from '../../store';
 import { InfiniteScrollTrigger } from '@tt/common-ui';
+// import { WaIntersectionObserver, WaIntersectionObservee } from '@ng-web-apis/intersection-observer';
+// import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
+import { firstValueFrom, scan, Subject } from 'rxjs';
+import { Profile, ProfileService } from '@tt/data-access';
 
 @Component({
   selector: 'tt-search-page',
-  imports: [ProfileCard, ProfileFilters, InfiniteScrollTrigger],
+  imports: [ProfileCard, ProfileFilters, InfiniteScrollTrigger/* WaIntersectionObserver, WaIntersectionObservee, InfiniteScrollDirective */],
   templateUrl: './search-page.html',
   styleUrl: './search-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchPage {
+export class SearchPage /* implements OnInit */ {
   store = inject(Store);
   profiles = this.store.selectSignal(selectFilteredProfiles);
+  /*   profileService = inject(ProfileService);
+    page = 0;
+  
+    profilesSubject$ = new Subject<Profile[]>();
+  
+    infiniteProfiles$ = this.profilesSubject$.pipe(
+      scan((acc, curr) => {
+        return acc.concat(curr) as Profile[];
+      }, [] as Profile[])
+    ); */
 
   constructor() {
     const currentFilters = this.store.selectSignal(selectProfileFilters)();
     this.store.dispatch(profileActions.filterEvents({ filters: currentFilters }));
   }
 
+  /*   ngOnInit() {
+      this.getNextPage();
+    }
+  
+    async getNextPage() {
+      this.page++;
+      const res = await firstValueFrom(this.profileService.filterProfiles({ page: this.page }));
+  
+      this.profilesSubject$.next(res.items);
+    } */
+
   timeToFetch() {
     this.store.dispatch(profileActions.setPage({}));
   }
+
+  /*   onIntersection(entries: IntersectionObserverEntry[]) {
+      if (!entries.length) return;
+  
+      if (entries[0].intersectionRatio > 0) {
+        this.timeToFetch();
+      }
+    } */
+
+  /*   onScroll() {
+      // this.timeToFetch();
+      this.getNextPage();
+    } */
 }
